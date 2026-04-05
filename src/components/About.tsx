d1335ca5fc5daa742a9144f94092ca0c4fa0c4b1
Khurshid Computers <tumhara-github-email@example.com>
2026-04-05 18:07:42 +0500

    push

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-        I’m Dameer Ijaz, a passionate Full Stack Web Developer specializing in React.js and Node.js. I build real-world projects that combine clean code, intuitive design, and seamless user experiences. I also explore AI-driven solutions to make apps smarter and more efficient. Always learning, always building—my goal is to turn ideas into functional, impactful digital experiences
+        I’m Dameer Ijaz, a passionate Full Stack Web Developer specializing in React.js and Node.js. I build real-world projects that combine clean code, intuitive design, and seamless user experiences. I also explore AI-driven solutions to make apps smarter and more efficient.
         </p>
       </div>
     </div>
